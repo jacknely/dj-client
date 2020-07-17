101d0bd7968a9292f8914fd1fe4c4625cc3acea4
Jack <jackn926@live.co.uk>
2020-07-17 15:55:08 +0100

    Updates

---
+++ precache-manifest.0586be9d2b8c7fc027e5e3ec3aa17687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52374969d78887ad649392e9504f7e28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1725669356b2fc444c71",
+    "url": "/static/css/main.f9d703af.chunk.css"
+  },
+  {
+    "revision": "3747dd335462b209f44a",
+    "url": "/static/js/2.d3ac4eab.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d3ac4eab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1725669356b2fc444c71",
+    "url": "/static/js/main.318ae5fd.chunk.js"
+  },
+  {
+    "revision": "df02bb9b8b63930cfe4b",
+    "url": "/static/js/runtime-main.8dffd7fd.js"
+  }
+]);
